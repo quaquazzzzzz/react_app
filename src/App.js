b0546d40dcd53495852669f664712d6b286b6b03
quaquazzzzz <annabawor.ab@googlemail.com>
2024-01-21 15:02:33 +0100

    Add welcome message and contact button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* Willkommensnachricht hinzufügen */}
+        <h1>Willkommen auf meiner Seite!</h1>
+        
+        {/* Button hinzufügen */}
+        <button>Kontakt</button>
+
+        {/* Rest des Codes entfernen oder kommentieren, falls nicht mehr benötigt */}
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,10 +22,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
 }
 
 export default App;
+
